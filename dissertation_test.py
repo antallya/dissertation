034282254b36684c3071c750afb20e7e960d4148
antallya <antalymp@gmail.com>
2024-08-25 19:45:26 +0100

    binomial distribution sigmoid intersection with empirical probabilities

--- dissertation_test.py
+++ dissertation_test.py
@@ -5,18 +5,27 @@ import seaborn as sns
 from scipy.stats import binom
 
 df=pd.read_csv("/home/antalya/CLionProjects/AntalyasGuesser/sampleFiles/results.csv",
-               header=None, usecols=list(range(0,128)))
+               header=None, usecols=list(range(0,32)))
 print(df.head())
 n_values = [5,10,50,100,500,750,1000]
 # Setting theme
 sns.set_theme()
-xvalues=list(range(0,128,8))
+xvalues=list(range(0,32,8))
 # Function for plotting each row of the dataframe
 for index, row in df.iterrows():
     # plot
     plt.figure(figsize=(8,4))
     sns.lineplot(x=row.index, y=row.values)
 
+    # binomial distribution
+    for i, column in enumerate(row.index):
+        n = n_values[index]  # Use corresponding n value based on row index
+        p = 1 / (2**i)  # Probability for the binomial distribution
+        binom_value = binom.pmf(k=int(n*p), n=n, p=p)
+
+        # Plot the binomial point
+        plt.scatter(column, binom_value, color='red', label=f'Binom (n={n}, p=1/2^k)' if i == 0 else "")
+        
     # labels and axis
     plt.xticks(xvalues)
     plt.xlabel('Number of bytes (8 bits) analysed')
@@ -28,4 +37,6 @@ for index, row in df.iterrows():
     plt.show()
 
 #sns.relplot(data=df, x="probabilities", y="")
+
+
 
