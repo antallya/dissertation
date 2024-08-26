32ff84ad94ab093db13c13eb19a1c1ebad3ec9f6
antallya <antalymp@gmail.com>
2024-08-26 17:23:27 +0100

    all lineplots for 64 bits analysis

---
+++ all_line_plots.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import binom
+
+df = pd.read_csv("/home/antalya/CLionProjects/AntalyasGuesser/sampleFiles/results.csv",
+                 header=None, usecols=list(range(0,64)))
+# number of hashes per experiment
+n_values = [5, 10, 100, 500, 750, 1000]
+# temporarily adding n to df
+df['n'] = n_values
+
+sns.set_theme()
+xvalues = list(range(0, 64))
+
+plt.figure(figsize=(12,8))
+for index, row in df.iterrows():
+    sns.lineplot(x=xvalues, y=row.drop('n').values, label=f'n = {row["n"]}')
+
+plt.xticks(list(range(0, 64, 2)))
+plt.xlabel('Number of bits analysed')
+plt.ylabel('Probability')
+plt.title('Probability of getting a zero per bit space analysed')
+plt.legend(title='Number of Hashes')
+
+plt.show()
+
