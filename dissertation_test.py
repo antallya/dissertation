3c9312fa9540ea6932be4b877c197b046667a854
antallya <antalymp@gmail.com>
2024-08-31 21:14:15 +0100

    changed fstrings

--- dissertation_test.py
+++ dissertation_test.py
@@ -15,32 +15,32 @@ xvalues=list(range(4,19))
 for index, row in df.iterrows():
     # empirical distribution of proportion of zeros
     plt.figure(figsize=(12, 6))
-    sns.lineplot(x=xvalues, y=row.values, color='navy', label='Average Proportion of Zeros')
+    sns.lineplot(x=xvalues, y=row.values, color='navy', label=f'Average Proportion of Zeros (Sample Size: {n_values[index]})')
     plt.axhline(y=0.5, color='teal', linestyle='--', label='Expected Proportion (0.5)')
 
     plt.xticks(xvalues)
     plt.xlabel('Number of bits analysed (p)')
     plt.ylabel('Proportion of Zeros')
-    plt.title('Average Proportion of Zeros per p bits analysed')
+    plt.title(f'Average Proportion of Zeros per p bits analysed (Sample Size: {n_values[index]})')
     plt.legend()
-    #plt.savefig(f'empirical_proportion_{index}.png')
+    plt.savefig(f'empirical_proportion_{index}.png')
     plt.show()
 
     # deviation from 0.5
     plt.figure(figsize=(12, 6))
     deviation_from_05 = np.abs(row.values - 0.5)
-    sns.lineplot(x=xvalues, y=deviation_from_05, color='red', label='Deviation from 0.5')
+    sns.lineplot(x=xvalues, y=deviation_from_05, color='red', label=f'Deviation from 0.5 (Sample Size: {n_values[index]})')
 
-    threshold = 0.05  # threshold error for small devs
+    threshold = 0.01  # threshold error for small devs
     small_deviation_indices = np.where(deviation_from_05 < threshold)[0]
     plt.scatter(x=np.array(xvalues)[small_deviation_indices],
-                y=deviation_from_05[small_deviation_indices], color='orange', label='Small Deviations')
+                y=deviation_from_05[small_deviation_indices], color='orange', label=f'Small Deviations (aprox: {threshold})')
 
     plt.xticks(xvalues)
     plt.xlabel('Number of bits analysed (p)')
     plt.ylabel('Deviation from 0.5')
-    plt.title('Deviation of Proportion of Zeros from 0.5')
+    plt.title(f'Deviation of Proportion of Zeros from 0.5 (Sample Size: {n_values[index]})')
     plt.legend()
-    #plt.savefig(f'deviation_{index}.png')
+    plt.savefig(f'deviation_{index}.png')
     plt.show()
 
